42e6d8b6c0e0e8a79d3bf75cb9d379d6d760c768
Dariusz Kralewski <dariusz.kralewski@ug.edu.pl>
2026-04-08 14:35:14 +0200

    reactivity + daisy UI

---
+++ src/views/TempView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import { users } from '@/assets/users'
+
+//#region  USER
+const user = ref({ LastName: 'Jednorowicz' })
+setTimeout(() => {
+  user.value = { LastName: '<h1>Jednorowicz</h1>' }
+}, 4000)
+//#endregion USER
+
+//#region  MOD
+const whySad = ref('Szaroburo')
+
+//#endregion  MOD
+</script>
+
+<template>
+  <input v-model.lazy.trim="whySad" />
+  <input v-model="whySad" />
+  <div>{{ user.LastName }}</div>
+  <div v-html="user.LastName"></div>
+  <pre>{{ users }}</pre>
+</template>
