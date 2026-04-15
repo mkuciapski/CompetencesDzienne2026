0461ff5e97ff686df341c64b69f93d9c11dc2e99
mkuciapki <m.kuciapski@gmail.com>
2026-04-15 14:08:58 +0200

    computed initial

--- src/views/CompetenciesView.vue
+++ src/views/CompetenciesView.vue
@@ -4,6 +4,8 @@ import { ref } from 'vue'
 
 const _competences = ref(competences)
 const _searchText = ref('')
+const _padding = ref('5px')
+
 setTimeout(() => {
   _competences.value[0]!.Description = 'Wolę Tailwinda'
   _competences.value.push({ Id: 11, Name: 'C++', Level: 4, Picture: 'bootstrap.svg', Description: 'Wersja 3' })
@@ -15,9 +17,9 @@ setTimeout(() => {
     <button class="btn join-item text-white bg-green-800">Podaj nazwę kompetencji</button>
     <input type="text" v-model="_searchText" />
   </div>
-  <div class="flex flex-wrap justify-center gap-3">
+  <div class="flex flex-wrap justify-center">
     <template v-for="competence in _competences" :key="competence.Id">
-      <div v-if="competence.Level" class="card bg-base-100 w-48 shadow-sm">
+      <div v-if="competence.Level" class="card bg-base-100 w-48 shadow-sm card-spacing">
         <h2 class="text-center text-xl uppercase">{{ competence.Name }}</h2>
         <img :src="`assets/${competence.Picture}`" />
         <div class="card-body">
@@ -28,3 +30,9 @@ setTimeout(() => {
     </template>
   </div>
 </template>
+
+<style scoped>
+.card-spacing {
+  margin: v-bind('_padding');
+}
+</style>
--- src/views/TempView.vue
+++ src/views/TempView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from 'vue'
 import { users } from '@/assets/users'
 
 //#region  USER
@@ -17,14 +17,28 @@ const second = ref('12px')
 setInterval(() => {
   second.value = new Date().getSeconds() + 'px'
 }, 4000)
+
+const _users = ref(users)
+const _bestUsers = computed((previous) => {
+  console.log('previous users', previous)
+  return _users.value.filter((user) => user.Score > 55)
+})
+
+setTimeout(() => {
+  _users.value[2]!.Score = 60
+}, 14000)
 </script>
 
 <template>
-  <input v-model.lazy.trim="whySad" />
+  <pre>{{ _bestUsers }}</pre>
+
+  <!-- #region OLD -->
+  <!-- <input v-model.lazy.trim="whySad" />
   <input v-model="whySad" />
   <div class="last-name-style">{{ user.LastName }}</div>
   <div v-html="user.LastName"></div>
-  <pre>{{ users }}</pre>
+  <pre>{{ users }}</pre> -->
+  <!-- #endregion OLD -->
 </template>
 
 <style scoped>
